bc205ffe1620626005b733d03c9eff2c6994277c
Bruna Antunes <profbrunaantunes@gmail.com>
2021-02-06 15:05:00 -0300

    alterando mensagem de apresentação; incluindo CIDADE

--- meu projeto/src/Main.java
+++ meu projeto/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá,mundo! Meu nome é Bruna. Tenho 29 anos e estou adorando aprender a linguagem Java");
+        System.out.println("Olá,mundo! Meu nome é Bruna. Moro em Porto Alegre.Tenho 29 anos e estou adorando aprender a linguagem Java");
 
     }
 }
